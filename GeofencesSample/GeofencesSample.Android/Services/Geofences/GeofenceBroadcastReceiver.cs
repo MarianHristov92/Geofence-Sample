c397ddbbfebfe056372653c63ae428f0a938465c
Marian Hristov <marian.hristov@keoz.com>
2021-07-26 09:36:06 +0200

    test

---
+++ GeofencesSample/GeofencesSample.Android/Services/Geofences/GeofenceBroadcastReceiver.cs
@@ -0,0 +1,47 @@
+﻿namespace GeofencesSample.Droid.Services.Geofences
+{
+	using Android.Content;
+    using Android.Gms.Location;
+    using GeofencesSample.Droid.Services.Notifications;
+    using System;
+    using System.Collections.Generic;
+
+    [BroadcastReceiver()]
+	public class GeofenceBroadcastReceiver : BroadcastReceiver
+	{
+		public override void OnReceive(Context context, Intent intent)
+		{
+			GeofencingEvent geofencingEvent = GeofencingEvent.FromIntent(intent);
+			if (geofencingEvent.HasError)
+			{
+				string error = GeofenceStatusCodes.GetStatusCodeString(geofencingEvent.ErrorCode);
+				Console.WriteLine($"Error Code: {geofencingEvent.ErrorCode}. Error: {error}");
+				return;
+			}
+
+			// Get the transition type.
+			int geofenceTransition = geofencingEvent.GeofenceTransition;
+			// Get the geofences that were triggered. A single event can trigger
+			// multiple geofences.
+			IList<IGeofence> triggeringGeofences = geofencingEvent.TriggeringGeofences;
+			NotificationsService notificationsService = new NotificationsService(context);
+
+			switch (geofenceTransition)
+			{
+				case Geofence.GeofenceTransitionEnter:
+					notificationsService.SendNotification("¡Enter!", "iWe have caught you close!", 10000);
+					break;
+				case Geofence.GeofenceTransitionExit:
+					notificationsService.SendNotification("¡You went out!", "¡We caught you coming out!", 10000);
+					break;
+				case Geofence.GeofenceTransitionDwell:
+					notificationsService.SendNotification("¡You stay!", "¿¡You don't move!?", 10000);
+					break;
+				default:
+					// Log the error.
+					Console.WriteLine("Broadcast not implemented.");
+					break;
+			}
+		}
+	}
+}
---
+++ GeofencesSample/GeofencesSample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace GeofencesSample
+{
+	using GeofencesSample.Services.Geofences;
+	using System;
+	using System.Collections.Generic;
+	using Xamarin.Essentials;
+	using Xamarin.Forms;
+	using Xamarin.Forms.Maps;
+
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+
+			IGeofencesService geofencesService = DependencyService.Get<IGeofencesService>();
+
+			List<Position> list = GetGeofences();
+			Location location = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(3)));
+			geofencesService.SetGeofences(list, new Position(location.Latitude, location.Longitude));
+		}
+
+		private List<Position> GetGeofences()
+		{
+			return new List<Position>
+			{
+				new Position(51.174975, 6.391762), // EDEKA
+				new Position(51.182997, 6.412036), // Kaufland
+				new Position(51.208155, -6.460733), // Real
+				new Position(0, 0),
+			};
+		}
+	}
+}
